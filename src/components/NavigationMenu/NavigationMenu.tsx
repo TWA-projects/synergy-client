5a3eca17a89e2a4abe673561bb9a96f050ce5a2f
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2024-04-24 02:13:55 +0300

    update nav menu

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -13,38 +13,38 @@ import './NavigationMenu.css';
 
 const NavigationMenu: React.FC = () => {
   return (
-    <div className='tabs is-centered navigationMenu'>
-      <ul>
+    <div className='tabs is-toggle is-centered navigationMenu'>
+      <ul className='is-flex'>
         <li className='is-active'>
-          <button>
-            <span className='icon is-small'>
+          <button className='navButton'>
+            <span className='icon is-medium'>
               <i className='fas fa-image' aria-hidden='true'></i>
             </span>
-            <span>Pictures</span>
+            {/* <span>Pictures</span> */}
           </button>
         </li>
         <li>
-          <button>
-            <span className='icon is-small'>
+          <button className='navButton'>
+            <span className='icon is-medium'>
               <i className='fas fa-music' aria-hidden='true'></i>
             </span>
-            <span>Music</span>
+            {/* <span>Music</span> */}
           </button>
         </li>
         <li>
-          <button>
-            <span className='icon is-small'>
+          <button className='navButton'>
+            <span className='icon is-medium'>
               <i className='fas fa-film' aria-hidden='true'></i>
             </span>
-            <span>Videos</span>
+            {/* <span>Videos</span> */}
           </button>
         </li>
         <li>
-          <button>
-            <span className='icon is-small'>
+          <button className='navButton'>
+            <span className='icon is-medium'>
               <i className='far fa-file-alt' aria-hidden='true'></i>
             </span>
-            <span>Documents</span>
+            {/* <span>Documents</span> */}
           </button>
         </li>
       </ul>
