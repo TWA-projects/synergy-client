5a8c8a8c23f9da94ca65e3321368656ff7e7cbc2
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2024-04-24 21:26:23 +0300

    update styles

--- src/pages/PageThree.tsx
+++ src/pages/PageThree.tsx
@@ -5,7 +5,13 @@ export const PageThree = () => {
   return (
     <MotionWrapper title='Page 1'>
       <div>
-        <img src={giphy} alt='Vite logo' />
+        <img
+          src={giphy}
+          alt='Vite logo'
+          style={{
+            borderRadius: '50%',
+          }}
+        />
       </div>
     </MotionWrapper>
   );
